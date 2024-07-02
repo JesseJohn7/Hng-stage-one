dec13f01d2d734972832bd4ee52285090cb28b8f
JesseJohn7 <143970775+JesseJohn7@users.noreply.github.com>
2024-07-02 22:01:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function updateTimeAndDay() {
+    const currentTimeUTC = new Date().toUTCString();
+    const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+
+    document.querySelector('[data-testid="currentTimeUTC"]').textContent = currentTimeUTC;
+    document.querySelector('[data-testid="currentDay"]').textContent = currentDay;
+}
+
+updateTimeAndDay();
+setInterval(updateTimeAndDay, 60000);
